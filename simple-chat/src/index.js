800708f555fcca172e970c5180dafe9d15f341d6
have2go <boxadmail@yandex.ru>
2024-09-30 13:39:04 +0300

    ДЗ_2 на проверку v3

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -1,53 +1,106 @@
 import "./index.css";
 
 const form = document.querySelector("form");
-const input = document.querySelector(".form-input");
+const input = document.querySelector(".form__input");
+const messages = document.querySelector(".messages");
+const testBtn = document.querySelector(".test-button");
+
+const months = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+];
+
+const MESSAGE_LIST_KEY = "messageArr";
+const USER = "Alex";
+
+let messagesToRender = [];
+let newMessages = [];
 
 const messageTemplate = obj => {
+    const creationDate = new Date(obj.createdAt);
+
+    const day = String(creationDate.getDate()).padStart(2, "0");
+    const month = months[creationDate.getMonth()];
+    const year = creationDate.getFullYear();
+    const hours = String(creationDate.getHours()).padStart(2, "0");
+    const minutes = String(creationDate.getMinutes()).padStart(2, "0");
+    const seconds = String(creationDate.getSeconds()).padStart(2, "0");
+
     return `
-    <div class="message">
+    <div class="message ${obj.author === USER ? "" : "message_incoming"}">
         <p class="message__text">${obj.message}</p>
-        <div class="message__info">
-            <p class="message__time">${obj.author}</p>
-            <p class="message__time">${obj.createdAt}</p>
-            <span class="material-symbols-outlined message__arrows"> done_all </span>
+        <div class="message__info tooltip">
+        <span class="tooltiptext">${day} ${month} ${year}г., ${hours}:${minutes}:${seconds}</span>
+            <p class="message__time">${hours + ":" + minutes}</p>
+            <span class="material-symbols-outlined message__arrows ${
+                obj.author === USER ? "" : "message__arrows_incoming"
+            }"> done_all </span>
         </div>
     </div>
     `;
 };
 
-let messagesToRender = [];
-
 if (localStorage.messageArr) {
-    messagesToRender = JSON.parse(localStorage.getItem("messageArr"));
+    messagesToRender = JSON.parse(localStorage.getItem(MESSAGE_LIST_KEY));
 }
 
 form.addEventListener("submit", handleSubmit);
-form.addEventListener("keypress", handleKeyPress);
+
+testBtn.onclick = () => sendTestMessage();
+function sendTestMessage() {
+    const date = new Date();
+    newMessages.push({
+        message: "Тестовое сообщение",
+        createdAt: date,
+        author: "Bob",
+    });
+}
 
 function handleSubmit(event) {
     event.preventDefault();
+    sendMessage();
+}
+
+function sendMessage() {
     if (input.value) {
         const date = new Date();
         messagesToRender.push({
             message: input.value,
-            createdAt: String(date.getHours()).padStart(2, "0") + ":" + String(date.getMinutes()).padStart(2, "0"),
-            author: "Алексей",
+            createdAt: date,
+            author: USER,
         });
-        localStorage.setItem("messageArr", JSON.stringify(messagesToRender));
+        localStorage.setItem(MESSAGE_LIST_KEY, JSON.stringify(messagesToRender));
         input.value = "";
         render();
+        messages.scrollTo({
+            top: 0,
+        });
     }
 }
 
-function handleKeyPress(event) {
-    if (event.keyCode === 13) {
-        form.dispatchEvent(new Event("submit"));
+setInterval(() => {
+    if (newMessages.length > 0) {
+        messagesToRender.push(...newMessages);
+        newMessages = [];
+        const sortedMessages = messagesToRender.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+        localStorage.setItem(MESSAGE_LIST_KEY, JSON.stringify(sortedMessages));
+        render();
     }
-}
+}, 500);
 
 function render() {
     document.querySelector(".messages").innerHTML = messagesToRender.map(messageTemplate).reverse().join("");
 }
 
 render();
+// localStorage.clear();
